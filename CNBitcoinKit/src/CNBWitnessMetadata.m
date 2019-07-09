b304f31297364d4c688d47c1b8b3e815fddeaf09
BJ Miller <bj.miller@coinninja.com>
2019-07-09 09:09:26 -0400

    Add Pieter Wuille cpp impelementation for bech32

---
+++ CNBitcoinKit/src/CNBWitnessMetadata.m
@@ -0,0 +1,13 @@
+//
+//  CNBWitnessMetadata.m
+//  CNBitcoinKit
+//
+//  Created by BJ Miller on 7/9/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+#import "CNBWitnessMetadata.h"
+
+@implementation CNBWitnessMetadata
+
+@end
